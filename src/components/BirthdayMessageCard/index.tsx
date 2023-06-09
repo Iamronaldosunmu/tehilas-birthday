feff9500ea91b26592f25789304c8634c24fa5b5
Ronald Dosunmu <76405995+Iamronaldosunmu@users.noreply.github.com>
2023-06-09 23:16:25 +0100

    Finished adding the animations

--- src/components/BirthdayMessageCard/index.tsx
+++ src/components/BirthdayMessageCard/index.tsx
@@ -1,22 +1,39 @@
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
+
 interface BirthdayMessageCardProps {
-    text: string;
-    image: string;
-    name: string;
+  text: string;
+  image: string;
+  name: string;
 }
 
-const BirthdayMessageCard : React.FC<BirthdayMessageCardProps> = ({text, image, name}) => {
+const BirthdayMessageCard: React.FC<BirthdayMessageCardProps> = ({
+  text,
+  image,
+  name,
+}) => {
+  const cardRef = useRef<HTMLDivElement>(null);
+  const isInView = useInView(cardRef, { once: true, amount: 0.8 });
   return (
-    <div className="w-full max-w-[400px] mx-auto bg-[#303236] pt-[25px] py-[20px] px-[20px] rounded-[10px] text-[14px] syneFont font-semibold text-[white] text-center">
+    <motion.div
+      animate={{
+        opacity: isInView ? 1 : 0,
+        scale: isInView ? 1 : 0.95,
+        transition: { duration: 2, delay: 1 },
+      }}
+      ref={cardRef}
+      className="w-full max-w-[400px] mx-auto bg-[#303236] pt-[25px] py-[20px] px-[20px] rounded-[10px] text-[14px] syneFont font-semibold text-[white] text-center"
+    >
       <div>
-        {text.split("\n").map((text: string) => (<p className="mt-[5px]">{text}</p>))}
-          </div>
-          <div className="mt-[35px] flex flex-col items-center">
-              <img src={image} className="w-[40px] h-[40px] rounded-full" />
-              <p className="mt-[15px]">
-                  - {name}
-              </p>
-          </div>
-    </div>
+        {text.split("\n").map((text: string) => (
+          <p className="mt-[5px]">{text}</p>
+        ))}
+      </div>
+      <div className="mt-[35px] flex flex-col items-center">
+        <img src={image} className="w-[40px] h-[40px] rounded-full" />
+        <p className="mt-[15px]">- {name}</p>
+      </div>
+    </motion.div>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,28 @@
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+import { motion } from "framer-motion";
+
 interface HeaderProps {
   text: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ text }) => {
-  return <div className="text-center text-[white] syneFont text-[25px] font-semibold mt-[50px]"> {text} </div>;
+  const headerRef = useRef<HTMLDivElement>(null);
+  const isInView = useInView(headerRef, { once: true, amount: 0.8 });
+  return (
+    <motion.div
+      animate={{
+        opacity: isInView ? 1 : 0,
+        scale: isInView ? 1 : 0.95,
+        transition: { duration: 2, delay: 1 },
+      }}
+      ref={headerRef}
+      className="text-center text-[white] syneFont text-[25px] font-semibold mt-[50px]"
+    >
+      {" "}
+      {text}{" "}
+    </motion.div>
+  );
 };
 
 export default Header;
